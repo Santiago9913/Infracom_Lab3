834e4f17a6122bfc4956d7ae2f7a24f20308b153
Santiago Fajardo <s.fajardot@uniandes.edu.co>
2020-10-05 22:22:44 -0500

    Correccion TCP

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,28 +1,9 @@
 package Client;
 
-import org.bouncycastle.asn1.x500.X500Name;
-import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
-import org.bouncycastle.cert.X509CertificateHolder;
-import org.bouncycastle.cert.X509v3CertificateBuilder;
-import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.operator.ContentSigner;
-import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
 import java.io.*;
-import java.math.BigInteger;
 import java.net.Socket;
-import java.security.*;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
-import java.util.Calendar;
+
 
 public class Client {
 
@@ -30,50 +11,22 @@ public class Client {
     private static final String OK = "OK";
     private static final String PADDING_AES = "AES/ECB/PKCS5Padding";
 
-    /**
-     * El puerto en el cual escucha el servidor
-     */
+
     private int puerto;
 
     private String host;
 
-    /**
-     * Con este socket conectamos al servidor usando: socket = new Socket(host, portNumber)
-     */
+
     private Socket socket;
 
-    /**
-     * Para escribir datos primitivos en texto
-     */
+
     private PrintWriter pw;
 
-    /**
-     * Para leer lo que se escribe por parte del servidor
-     */
+
     private BufferedReader brServer;
 
-    /**
-     * Para leer lo que escribe el cliente
-     */
+
     private BufferedReader brCliente;
-
-    private String[] algoritmos;
-
-    private String simetrico;
-
-    private String asimetrico;
-
-    private String hash;
-
-    private PrivateKey privateKey;
-
-    private PublicKey publicKey;
-
-    private PublicKey publicKeyServidor;
-
-    private KeyPair keyPairCliente;
-
-    private SecretKey sessionKey;
 
     private int id;
 
@@ -100,85 +53,7 @@ public class Client {
         return DatatypeConverter.parseBase64Binary(s);
     }
 
-    private KeyPair generarLlaves() throws Exception{
-        KeyPairGenerator keygen = KeyPairGenerator.getInstance("RSA");
-        keygen.initialize(1024);
-        KeyPair keyPair = keygen.generateKeyPair();
-        this.privateKey = keyPair.getPrivate();
-        this.publicKey = keyPair.getPublic();
-        return keyPair;
-    }
 
-    public static X509Certificate generarCertificado(KeyPair keyPair) throws Exception{
-
-        Calendar endCalendar = Calendar.getInstance();
-        endCalendar.add(Calendar.YEAR, 10);
-        X509v3CertificateBuilder x509v3CertificateBuilder = new X509v3CertificateBuilder(
-                new X500Name("CN=cliente"),
-                BigInteger.valueOf(1),
-                Calendar.getInstance().getTime(),
-                endCalendar.getTime(),
-                new X500Name("CN=cliente"),
-                SubjectPublicKeyInfo.getInstance(keyPair.getPublic().getEncoded())
-        );
-        ContentSigner contentSigner = new JcaContentSignerBuilder("HMACSHA512 with RSA").build(keyPair.getPrivate());
-        X509CertificateHolder x509CertificateHolder = x509v3CertificateBuilder.build(contentSigner);
-        return (new JcaX509CertificateConverter().setProvider(new BouncyCastleProvider()).getCertificate(x509CertificateHolder));
-    }
-
-    private boolean verificarCertificado(X509Certificate certificado) throws CertificateException, NoSuchAlgorithmException, NoSuchProviderException, BadPaddingException {
-        try {
-            this.publicKeyServidor = certificado.getPublicKey();
-            certificado.verify(this.publicKeyServidor);
-            return true;
-        } catch (SignatureException e){
-            e.printStackTrace();
-            return false;
-        } catch (InvalidKeyException e2){
-            e2.printStackTrace();
-            return false;
-        }
-    }
-
-    private SecretKey descifrarLlaveServidorCliente(byte[] llave) throws Exception{
-        byte[] descifrado;
-
-        Cipher cifrado = Cipher.getInstance("RSA");
-        cifrado.init(cifrado.DECRYPT_MODE, privateKey);
-        descifrado = cifrado.doFinal(llave);
-        SecretKey key = new SecretKeySpec(descifrado, "AES");
-        return key;
-
-    }
-
-    private String descifrarMensajeSimetricoServidorCliente(byte[] textoCifrado) throws Exception{
-        byte[] textoDescifradoEnBytes;
-        Cipher cifrado = Cipher.getInstance(PADDING_AES);
-        cifrado.init(cifrado.DECRYPT_MODE, sessionKey);
-        textoDescifradoEnBytes = cifrado.doFinal(textoCifrado);
-        return toHexString(textoDescifradoEnBytes);
-    }
-
-    private byte[] cifrarSimetrico(Key llavePublicaServidor, String mensaje) throws Exception{
-        byte[] mensajeCifrado;
-
-        Cipher cifrado = Cipher.getInstance("RSA");
-        byte[] mensajeBytes = toByteArray(mensaje);
-
-        cifrado.init(Cipher.ENCRYPT_MODE,llavePublicaServidor);
-        mensajeCifrado = cifrado.doFinal(mensajeBytes);
-        return mensajeCifrado;
-    }
-
-    private byte[] cifrarSimetricamente(Key llaveCompartida, String mensaje) throws Exception{
-        byte[] mensajeCifrado;
-        Cipher cifrado = Cipher.getInstance(PADDING_AES);
-        byte[] mensajeClaro = toByteArray(mensaje);
-
-        cifrado.init(Cipher.ENCRYPT_MODE, llaveCompartida);
-        mensajeCifrado = cifrado.doFinal(mensajeClaro);
-        return mensajeCifrado;
-    }
 
     public void run() throws Exception{
         while(true){
@@ -187,28 +62,8 @@ public class Client {
             String msjServidor = brServer.readLine();
 
             if(msjServidor.equals(OK)){
-                keyPairCliente = generarLlaves();
-                X509Certificate certificado = generarCertificado(keyPairCliente);
-                byte[] certificadoBytes = certificado.getEncoded();
-                String certificadoString = toHexString(certificadoBytes);
-                pw.println(certificadoString);
+                pw.println(OK);
             }
-
-            //Guardar certificado del servidor
-            System.out.println("Reciviendo certificado del servidor...");
-            String certificadoSerrvidorStr = brServer.readLine();
-            byte[] certificadoServidorBytes = toByteArray(certificadoSerrvidorStr);
-            CertificateFactory cf = CertificateFactory.getInstance("X.509");
-            Certificate certificadoServidor = cf.generateCertificate(new ByteArrayInputStream(certificadoServidorBytes));
-            publicKeyServidor = certificadoServidor.getPublicKey();
-            pw.println(OK);
-
-            //Leer y descifrar la session key
-            String sessionKeyString = brServer.readLine();
-            byte[] sessionKeyBytes = toByteArray(sessionKeyString);
-            sessionKey = descifrarLlaveServidorCliente(sessionKeyBytes);
-
-
         }
     }
 
--- src/Server/ServerThread.java
+++ src/Server/ServerThread.java
@@ -2,39 +2,22 @@ package Server;
 
 import java.io.*;
 import java.net.Socket;
-import java.security.KeyPair;
-import java.security.cert.X509Certificate;
+
 
 public class ServerThread extends Thread {
 
     private static final String HOLA = "HOLA";
     private static final String OK = "OK";
     private static final String ERROR = "ERROR";
-    private static final String PADDING_AES = "AES/ECB/PKCS5Padding";
 
-    private static X509Certificate certificado;
-    private static KeyPair keyPair;
     private int id;
     private Socket socket;
-    private byte[] mybyte;
 
-
-    public static void init(X509Certificate cert, KeyPair pKeyPair){
-        certificado = cert;
-        keyPair = pKeyPair;
-    }
 
     public ServerThread(Socket socket, int id){
         this.id = id;
         this.socket = socket;
 
-        try {
-            mybyte = new byte[520];
-            mybyte = certificado.getEncoded();
-        } catch (Exception e) {
-            System.out.println("Error creando el thread");
-            e.printStackTrace();
-        }
     }
 
     public void run(){
@@ -48,7 +31,6 @@ public class ServerThread extends Thread {
             linea = bf.readLine();
             if(linea.equals(HOLA)){
                 pw.println(OK);
-                System.out.println("Conexion Exitosa Con Cliente...");
             } else{
                 pw.println(ERROR);
                 socket.close();
@@ -58,8 +40,5 @@ public class ServerThread extends Thread {
         } catch (Exception e){
             e.printStackTrace();
         }
-
-
-
     }
 }
